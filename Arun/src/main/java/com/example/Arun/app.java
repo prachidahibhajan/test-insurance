074bb89353e410e6ddd2325fe6e14672b6e51b74
Prachi <prachidahibhajan.123@gmail.com>
2023-06-01 15:15:03 +0530

    test code get commited

---
+++ Arun/src/main/java/com/example/Arun/app.java
@@ -0,0 +1,74 @@
+package com.example.Arun;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class app {
+	public static void main(String[] args) throws
+	InterruptedException, IOException{
+		{
+			 System.setProperty("webdriver.chrome.driver","C://Users//priya//eclipse//chromedriver_win32 (1)//chromedriver.exe");
+			 
+			 //System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+			
+			 
+			 //ChromeOptions chromeOptions = new ChromeOptions();
+			 //chromeOptions.addArguments("--headless");
+			 //chromeOptions.addArguments("--no-sandbox");
+			 //chromeOptions.addArguments("--disable-dev-shmusage");
+			 //chromeOptions.addArguments("--remote-alloworigins=*");
+			 WebDriver driver= new ChromeDriver();
+			 
+			 //driver.get("http://localhost:8084/contact.html");
+			 
+			 driver.get("http://3.85.62.188:8081/contact.html");
+			 
+			driver.manage().timeouts().implicitlyWait(5,TimeUnit.SECONDS);
+			 
+			 
+			driver.findElement(By.id("inputName")).sendKeys("Shubham Singh Kushwah");
+			 
+			driver.findElement(By.id("inputNumber")).sendKeys("9999999999");
+			 
+			driver.findElement(By.id("inputMail")).sendKeys("shubham@gmail.com");
+			 
+			driver.findElement(By.id("inputMessage")).sendKeys("Welcome to the DevOps");
+			 
+			 driver.findElement(By.id("my-button")).click();
+			 
+			 String message =
+			driver.findElement(By.id("response")).getText();
+			 if(message.equals("Message Sent")) {
+			 System.out.println("Script executed Successfully");
+			 }
+			 else {
+			 System.out.println("Script failed");
+			 }
+			 
+			 System.out.println("Taking Screenshot as proof");
+			 //take the screenshot of the testcase
+			 
+			/*
+			 * TakesScreenshot scrShot = 
+			((TakesScreenshot)driver);
+			 * 
+			 * File screenShot = 
+			scrShot.getScreenshotAs(OutputType.FILE);
+			 * 
+			 * File destFile = new File("//home//ubuntu//
+			screenshot.png");
+			 * 
+			 * FileUtils.copyFile(screenShot, destFile);
+			 */
+			 
+			 Thread.sleep(3000);
+			 
+			 driver.quit();
+		
+	}
+
+	}}
---
+++ Arun/src/test/java/com/example/Arun/ArunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Arun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
